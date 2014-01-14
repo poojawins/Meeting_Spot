8398fcb80c4d3b03144a6a0221d21ea5dc97c7ed
Pooja Pasawala <pooja@basicer.com>
2014-01-14 14:21:03 -0500

    Add maps controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,27 +5,38 @@ class MapsController < ApplicationController
   end
 
   def show
-    
+    @map = Map.find(params[:id])
   end
 
   def new
-
+    @map = Map.new
   end
 
   def create
-
+    @map = Map.new(params[:map])
+    if @map.save
+      redirect_to @map
+    else
+      render new
+    end
   end
 
   def edit
-
+    @map = Map.find(params[:id])
   end
 
   def update
-
+    @map = Map.find(params[:id])
+    if @map.update_attributes(params[:map])
+      redirect_to @map
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @map = Map.delete(params[:id])
   end
 
 end
+
