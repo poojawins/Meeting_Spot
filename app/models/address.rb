f144b765472213a5d8c4fbc73f598b1ebe005e3f
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-15 17:33:23 -0500

    Add geocoder to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ActiveRecord::Base
+	attr_accessible :place, :name
   belongs_to :map
-
+	geocoded_by :place   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
 end
