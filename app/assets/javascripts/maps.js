a142a9ba7bda13579cd9d3a6229bf15cf1568e82
Sara Gorecki <sgorecki@gmail.com>
2014-01-16 16:59:38 -0500

    remove unnecessary variables from maps.js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,10 +1,7 @@
-  var lat = 0;
-  var lon = 0;
-
-  function initialize(){
-    var mapProp = {
-      center:new google.maps.LatLng(lat, lon), zoom:12,
-      mapTypeID:google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-  }
+function initialize(){
+  var mapProp = {
+    center:new google.maps.LatLng(lat, lon), zoom:12,
+    mapTypeID:google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
