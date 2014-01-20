1bd208b5505cd8eb54ae9151a85ffe9441310238
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-20 11:31:14 -0500

    Update maps and address models with user info

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ActiveRecord::Base
 	attr_accessible :place, :name
   belongs_to :map
+  belongs_to :user
 	geocoded_by :place   # can also be an IP address
 	after_validation :geocode,  if: ->(obj){ obj.place.present? and obj.place_changed? } # auto-fetch coordinates
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,7 +1,9 @@
 class Map < ActiveRecord::Base
-  has_many :addresses
   attr_accessor :city
   attr_accessible :name, :middle_ground_lat, :middle_ground_long, :city
+  has_many :addresses
+  has_many :users, :through => :addresses
+
 
   accepts_nested_attributes_for :addresses
 
