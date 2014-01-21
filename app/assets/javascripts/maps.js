7276cd53925d3629d88ccacfe1bb505b2e18eaf0
CodeOatmeal <JahmilEady@gmail.com>
2014-01-21 12:00:14 -0500

    update formatting and remove unused variable

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,19 +1,18 @@
 var map;
-var myLatLng = new google.maps.LatLng(lat, lon);
 
 function initialize(){
   var mapProp = {
     center:new google.maps.LatLng(lat, lon), 
     zoom:12,
     mapTypeID:google.maps.MapTypeId.ROADMAP
-
   };
+  
   map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
 function addMarker(){
-    var newMark = new google.maps.Marker({
-    	position: new google.maps.LatLng(lat,lon), 
-    	map:map
-    });	
+  var newMark = new google.maps.Marker({
+  	position: new google.maps.LatLng(lat,lon), 
+  	map:map
+  });	
 }
