c25e7b6124615de632e72f78e6714856dcaa91de
Sara Gorecki <sgorecki@gmail.com>
2014-01-23 12:05:29 -0500

    set maximum zoom levels for map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,7 +4,8 @@ function initialize(){
   var mapProp = {
     center:new google.maps.LatLng(lat, lon), 
     zoom:12,
-    mapTypeID:google.maps.MapTypeId.ROADMAP
+    mapTypeID:google.maps.MapTypeId.ROADMAP,
+    maxZoom: 16
   };
   
   map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
