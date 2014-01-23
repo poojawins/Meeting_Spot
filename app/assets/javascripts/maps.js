260c8bc331a52e11cfeee3d45479642d57950ee5
CodeOatmeal <JahmilEady@gmail.com>
2014-01-23 18:29:00 -0500

    incorporate google API for direction results. Still working on feature.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,10 @@
 var map;
+var directionsService;
+var markerArray = [];
+var directionResults;
 
 function initialize(){
+  directionsService = new google.maps.DirectionsService();
   var mapProp = {
     center:new google.maps.LatLng(lat, lon), 
     zoom:12,
@@ -16,4 +20,27 @@ function addMarker(){
   	position: new google.maps.LatLng(lat,lon), 
   	map:map
   });	
+} 
+
+function calcRoute(startLoc, endLoc){
+  for (i = 0; i < markerArray.length; i++){
+    markerArray[i].setMap(null);
+  }
+
+  var request {
+    origin: startLoc,
+    destination: endLoc,
+    travelMode: google.maps.TravelMode.TRANSIT,
+    transitOptions:{
+      departureTime: new Date(2014,0,27)
+    }   
+  }
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionResults = response;
+    //   var warnings = document.getElementById("warnings_panel") 
+    //   warnings.innerHTML = " " + response.routes[0].warnings + " ";
+    //   directionsDisplay.setDirections(response); showSteps(response);
+    }
+  });
 }
