5cffafb77ead894718cc26a8026fd5addcc95035
Pooja Pasawala <pooja@basicer.com>
2014-01-29 15:37:04 -0500

    Add midpoint method in midWay function

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -48,9 +48,10 @@ function calcRoute(startLoc, endLoc){
 }
 
 function midWay(){
-  var midPoint = Math.floor(directionResults.routes[0].overview_path.length / 2);
-  var midLat = directionResults.routes[0].overview_path[midPoint].d;
-  var midLong = directionResults.routes[0].overview_path[midPoint].e;
+  var longestRoute = findLongestRoute();
+  var midPoint = Math.floor(longestRoute.routes[0].overview_path.length / 2);
+  var midLat = longestRoute.routes[0].overview_path[midPoint].d;
+  var midLong = longestRoute.routes[0].overview_path[midPoint].e;
   return [midLat, midLong];
 }
 
